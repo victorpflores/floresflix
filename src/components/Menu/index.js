f1b13f3ef1544c63b76ab3e963b3c1bd628cc056
Victor Flores <victorpflores@outlook.com>
2020-07-28 20:50:46 -0300

    Versão 1.0

---
+++ src/components/Menu/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import Logo from '../../assets/img/Logo.png';
+import './Menu.css';
+// import ButtonLink from './components/ButtonLink';
+import Button from '../Button';
+
+
+function Menu(){
+    return (
+        <nav className="Menu">
+            <Link to="/">
+                <img className="Logo" src={Logo} alt="FLORESFLIX"/>
+            </Link>
+            
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
+                Novo Vídeo
+            </Button>
+        </nav>
+    );
+}
+
+export default Menu;
