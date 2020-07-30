3a85969070a0089e319476fec339247eb7fe5ab5
Victor Flores <victorpflores@outlook.com>
2020-07-30 14:59:32 -0300

    Ajustado loading

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -74,6 +74,11 @@ function CadastroCategoria() {
 
         <Button>Cadastrar</Button>
       </form>
+      {categorias.length === 0 && (
+        <div>
+            Loading...
+        </div>
+      )}
       <ul>
         {categorias.map((categoria) => (
           <li key={`${categoria.nome}`}>
